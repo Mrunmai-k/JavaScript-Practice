df7a325bbd9b20f5e20ed3b0a91115fa1f37648f
mrunmai <mrunmaikudale18@gmail.com>
2023-07-31 23:47:16 +0530

    Created html and js files and learned datatypes in JavaScript

---
+++ index.js
@@ -0,0 +1,52 @@
+// 1. Ways to print in JavaScript
+// document.write("this is document write");
+// console.log("Hello Mrunmai");
+// alert("Hi");
+
+// 2. JavaScript Console API
+console.log("Hello Mrunmai", 4+6, "Another log");
+console.warn("This is a warning");
+console.error("This is an error"); 
+
+/* Multi line comment */ 
+
+// 3. JavaScript Variables
+// variables - containers to store data values
+var num1 = 34;
+var num2 = 45;
+console.log(num1 + num2);
+
+// 4. Data Types in JavaScript
+// String
+var str1 = 'This is a string';
+var str2 = "This is a string";
+
+// Numbers
+var no1 = 45;
+var frano = 54.65;
+
+// Object
+var marks = {
+    mrunami:24,
+    mrunal:56,
+    pramila:87
+}
+
+// Booleans
+var a = true;
+var b =false;
+
+// Undefined
+var und;
+
+// Null
+var n =null;
+
+// Arrays - collections of elements
+var arr = [1,2,3,4,5];
+
+/* At a very high level, there are two types of datatypes in JavaScript:
+1. Primitive data type : undefined,null,number,string,boolean,symbol
+2. Reference data type : arrays, objects
+*/
+
