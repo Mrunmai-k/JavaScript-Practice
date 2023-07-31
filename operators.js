61b42d082a35f2f0ba21de0bf83896409f02d241
mrunmai <mrunmaikudale18@gmail.com>
2023-08-01 00:10:41 +0530

    Added all operators notes

--- operators.js
+++ operators.js
@@ -3,3 +3,40 @@
 var a = 45;
 var b = 67;
 console.log("The addition of a and b is: ",a+b);
+console.log("The subtraction of a and b is: ",a-b);
+console.log("The multiplication of a and b is: ",a*b);
+console.log("The division of a and b is: ",a/b);
+
+// Assignment operator
+var c = b;
+c += 2;
+c -= 2;
+c *= 2;
+c /= 2;
+console.log(c);
+
+// Comparison operator
+var x = 34; 
+var y = 56;
+console.log(x == y);
+console.log(x <= y);
+console.log(x >= y);
+console.log(x > y);
+console.log(x < y);
+
+// Logical operator
+// Logical AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// Logical OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// Logical NOT
+console.log(!false);
+console.log(!true);
