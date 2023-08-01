290792f1a000186b525b68933d5250d9c0482b83
mrunmai <mrunmaikudale18@gmail.com>
2023-08-01 23:46:59 +0530

    Added notes of string methods in JavaScript

---
+++ stringmethods.js
@@ -0,0 +1,16 @@
+let str = "Mrunmai is a very very good girl";
+console.log(str);
+
+// length of the string 
+console.log(str.length);
+
+// first occurence
+console.log(str.indexOf("very"));
+
+// last occurence
+console.log(str.lastIndexOf("very"));
+
+console.log(str.slice(0, 7));
+
+console.log(str.replace("Mrunmai", "Mrunal"));
+// console.log(str);
