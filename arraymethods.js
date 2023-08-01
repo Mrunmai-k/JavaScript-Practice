f64e1aeee6d8be735c8fddfb9c307d8e7280652b
mrunmai <mrunmaikudale18@gmail.com>
2023-08-01 19:46:36 +0530

    Added notes of different array methods

---
+++ arraymethods.js
@@ -0,0 +1,27 @@
+let myArr = ["Fan","Camera",79,null,true];
+console.log(myArr);
+
+// length of the array
+// console.log(myArr.length);
+
+// deletes last element of the array
+// myArr.pop();
+
+// adds element at the end
+// myArr.push("Mrunmai");
+
+// deletes first element of the array 
+// myArr.shift();
+
+// adds element at the start
+// myArr.unshift("Start");
+
+// returns the length of the new array after adding the element
+// console.log(myArr.unshift("length"));
+
+// converts the array into string
+myArr.toString();
+
+// sorts the elements in the array
+myArr.sort();
+console.log(myArr);
