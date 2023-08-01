3689ba5e7ec9e6229ff56f6d1213835a22ceba01
mrunmai <mrunmaikudale18@gmail.com>
2023-08-01 23:48:35 +0530

    Added notes of date  methods in JavaScript

---
+++ datemethods.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
